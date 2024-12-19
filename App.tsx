c9aada2b784d81f0e8d5fdce17b60ee1fdadc00b
unknown <Administrator@SAPPHIRE-10.tricon.local>
2024-12-19 09:43:14 +0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+
+
+
+import { View, Text, Button } from 'react-native';
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize : 50 }}>Abdul Muqtdar</Text>
+      <Button title='Press Here' />
+    </View>
+  );
+};
+
+export default App;
